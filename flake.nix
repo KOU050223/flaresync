c95c43989e59348316b2e3c1cfbfc6dab1ec39a0
KOU050223 <kouhei20050223@icloud.com>
2026-04-21 04:33:54 +0900

    chore: Nix flake と direnv 設定を追加

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "flaresync - Cloudflare Durable Objects state sync library";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_24
+            pnpm
+            wrangler
+          ];
+        };
+      });
+}
